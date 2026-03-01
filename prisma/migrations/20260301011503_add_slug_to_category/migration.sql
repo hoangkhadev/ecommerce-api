5197cd9ab9fe8ed8a5fceafb952162a31446a53b
hoangkhadev <hoangkhadev@gmail.com>
2026-03-01 08:16:46 +0700

    feat(category): add slug and update category schema

---
+++ prisma/migrations/20260301011503_add_slug_to_category/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[slug]` on the table `categories` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `categories` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "categories_name_key";
+
+-- AlterTable
+ALTER TABLE "categories" ADD COLUMN     "slug" VARCHAR(80) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_slug_key" ON "categories"("slug");
